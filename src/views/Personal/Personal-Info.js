1975a1ab8d2ca2ee02ab99dce18dc4323a71462a
Allison Tovar <alis.th94@gmail.com>
2022-08-23 14:19:49 -0500

    The footer component was created

--- src/views/Personal/Personal-Info.js
+++ src/views/Personal/Personal-Info.js
@@ -1,10 +1,12 @@
-import { Aside } from "../../components/Aside";
+import { Aside } from "../../components/Aside/Aside";
+import { Footer }from "../../components/Footer/Footer";
 
 
 export const Personal = () => {
   return (
     <main>
     <Aside />
+    <Footer />
     </main>
   );
 };
