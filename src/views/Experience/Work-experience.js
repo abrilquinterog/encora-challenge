3a2ba5a5a64e539ee705283f2cab945a0e40afcc
abrilquinterog <abril_quintero@hotmail.com>
2022-08-24 10:47:48 -0500

    Adds routing and css module to the work experience component

--- src/views/Experience/Work-experience.js
+++ src/views/Experience/Work-experience.js
@@ -1,68 +1,72 @@
-import { Aside } from "../../components/Aside";
+import { Aside } from "../../components/Aside/Aside";
 import { useState } from "react";
-import "./Work-experience.css";
-//import { useNavigate } from "react-router-dom";
+import module from './Work-experience.module.css';
+import { useNavigate } from "react-router-dom";
 
 
 export const WorkExperience = () => {
-    /*const navigate = useNavigate();
+    const navigate = useNavigate();
 
-    const handleOnClick = () => {
-        navigate('/personal-info');*/
+    const handleOnClickBack = () => {
+        navigate('/options')};
+    
+    const handleOnClickNext = () => {
+        navigate('/education')};    
     
     const [technologies,setTechnologies]=useState(["JAVASCRIPT", "C++", "PYTHON", "HTML", "RUBY", 
     "SQL", "C#", "REACT JS", "NODE JS", "ANGULAR JS", "LAMP", "MEAN", "MEVN"]);
 
   return (
-    <main className="main-work">
-    <Aside />
-    <section className="form-container">
-        <h2 className="second-step">Step 2</h2>
-        <h1 className="work-tittle">Work Experience</h1>
-        <button className="tips"><i></i>TIPS</button>
-        <form className="form-work">
-            <label className="form-label">Company/Employer<br/>
+    <main className={module.mainWork}>
+    <section className={module.asideContainer}> <Aside/> </section>
+    <section className={module.formContainer}>
+        <h2 className={module.secondStep}>Step 2</h2>
+        <h2 className={module.workExpTittle}>Work Experience</h2>
+        <button className={module.tips}><i></i>TIPS</button>
+        <form className={module.formWork}>
+            <label className={module.formLabel}>Company/Employer<br/>
             <input type="text" placeholder="e.g. Encora"/><br/>
             </label>
 
             <label>Years at the company <br/>
-            <select className="selector">
+            <select className={module.selector}>
                 <option selected value="Start Year">Start Year</option>
             </select>
 
-            <select className="selector">
+            <select className={module.selector}>
                 <option selected value="End Year">End Year</option>
             </select>
             </label><br/>
 
             <label>Job Tittle<br/>
-            <input type="text" placeholder="e.g. Front End Engineer" className="form-input"/>
+            <input type="text" placeholder="e.g. Front End Engineer" className={module.formInput}/>
             </label>
 
             <label>Tech Stack</label><br/>
-            <div className="checkbox-tech">
+            <div className={module.checkboxTech}>
                 {technologies.map(technology=>{
                     return(
-                        <label className="tech-btn">
+                        <label className={module.techBtn}>
                         <input type="checkbox"/><span>{technology}</span>
                         </label>
                         )
                 })}
             </div><br/>
 
-            <button>ADD TECH STACK</button><br/>
+            <button className={module.addBtn}>ADD TECH STACK</button><br/>
 
             <label>Job Description<br/>
-            <input type="textarea" placeholder="Type job description..." className="form-textarea"/>
+            <input type="textarea" placeholder="Type job description..." className={module.formTextarea}/>
             </label>
         </form>
         
-        <div className="nav-container">
-        <button className="btn-nav">BACK</button>
-        <button className="btn-nav">NEXT</button>
+        <div className={module.navContainer}>
+        <button className={module.btnNav} onClick={handleOnClickBack}>BACK</button>
+        <button className={module.btnNav} onClick={handleOnClickNext}>NEXT</button>
         </div>
     </section>
    
     </main>
   );
 };
+
