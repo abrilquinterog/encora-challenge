2930fc4f03dbfd4b209da07a529b5085518b9ae2
nathaliesainz <nathaliesainz13@gmail.com>
2022-08-24 12:06:37 -0500

    Updated images names and styles modules.

--- src/views/Options/Options.js
+++ src/views/Options/Options.js
@@ -1,14 +1,14 @@
-//import { Aside } from '../../components/Aside';
-import './options.css';
-import { useState } from 'react';
+import { Aside } from "../../components/Aside/Aside";
+import style from './Options.module.css';
+import { Fragment, useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import img1 from '../../assets/Money Illustration.png'
-import img2 from '../../assets/Write Illustration.png'
-import gif1 from '../../assets/Personal-Project.gif'
-import gif2 from '../../assets/Work-Experience.gif'
+import projectsImage from '../../assets/Money Illustration.png'
+import projectsGif from '../../assets/Personal-Project.gif'
+import workImage from '../../assets/Write Illustration.png'
+import workGif from '../../assets/Work-Experience.gif'
 
 const Options = () => {
-    const [selected,setSelected] = useState(null)
+    const [selected, setSelected] = useState(null)
     const navigate = useNavigate();
 
     const handleOnClick = () => {
@@ -20,35 +20,34 @@ const Options = () => {
 
 
   return (
-    <main className="main">
-    {/* <Aside /> */}
+    <main className={style.optionsMain}>
+    {<Aside />}
 
-    <section className="content-section">
-        <container className="title-container">
-            <p>Step 2<br/>
-            Work Experience<br/>
-            Select one option<br/>
-            </p>
+    <section className={style.contentSection}>
+        <container className={style.titleContainer}>
+            Step 2
+            Work Experience
+            Select one option
         </container>
 
-        <container className="cards-container">
-            <button className="cards" onClick={()=>{
+        <container className={style.cardsContainer}>
+            <button className={style.cards} onClick={()=>{
                 handleSelected('first')
             }}>
                 Personal<br/> Projects<br/>
-                <img src={selected==='first'?gif1:img1} alt='Money Illustration'/>
+                <img className={style.projectsImage} src={selected==='first'?projectsGif:projectsImage} alt='Money Illustration'/>
             </button>
-            <button className="cards" onClick={()=>{
+            <button className={style.cards} onClick={()=>{
                 handleSelected('second')
             }}>
                 Work<br/> Experience
-                <img  src={selected==='second'?gif2:img2} alt='Money Illustration'/>
+                <img className={style.workImage} src={selected==='second'?workGif:workImage} alt='Money Illustration'/>
             </button>
         </container>
 
-        <container className="buttons-container">
-            <button className="back-button" onClick={handleOnClick}>BACK</button>
-            <button className="next-button" >NEXT</button>
+        <container className={style.buttonsContainer}>
+            <button className={style.backButton} onClick={handleOnClick}>BACK</button>
+            <button className={style.nextButton} >NEXT</button>
         </container>
     </section>
 
